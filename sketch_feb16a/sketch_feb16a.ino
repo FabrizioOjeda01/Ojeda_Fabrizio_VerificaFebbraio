e4a3a329988d644c719e1595e04f5b5eb02538ba
Fabrizio <fabrizioojeda@hotmail.it>
2019-02-16 08:51:19 +0100

    completato metodo di accensione dei led

--- sketch_feb16a/sketch_feb16a.ino
+++ sketch_feb16a/sketch_feb16a.ino
@@ -1,11 +1,12 @@
-int rossso = 12;
+int rosso = 12;
 int giallo = 6;
 int blu    = 8;
 int verde  = 10;
-
+int tempoAccens = 0;
 
 void setup() {
   // put your setup code here, to run once:
+Serial.begin(9600);
 pinMode(rosso, OUTPUT);
 pinMode(giallo, OUTPUT);
 pinMode(blu, OUTPUT);
@@ -14,5 +15,18 @@ pinMode(verde, OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+Sequenza(rosso, verde, giallo, blu);
+delay(2000);
+Sequenza(giallo, blu, rosso, verde);
+delay(2000);
 }
+
+void Sequenza(int led, int led1, int led2, int led3){
+digitalWrite(led, LOW);
+digitalWrite (led1, LOW);
+digitalWrite (led2, HIGH);
+digitalWrite (led3, HIGH);
+}
+
+
+
