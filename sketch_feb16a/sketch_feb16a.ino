ff2dc4f9f5d19947da44bf5d90cbde58b60f40ac
Fabrizio <fabrizioojeda@hotmail.it>
2019-02-16 09:06:18 +0100

    Completamento del programma, con commenti

--- sketch_feb16a/sketch_feb16a.ino
+++ sketch_feb16a/sketch_feb16a.ino
@@ -1,8 +1,14 @@
+//prime variabili
 int rosso = 12;
-int giallo = 6;
-int blu    = 8;
-int verde  = 10;
-int tempoAccens = 0;
+int verde = 10;
+
+//seconde variabili
+int blu    = 8; 
+int giallo  = 6;
+
+//variabili per la durata
+int tempoRossoVerde = 0;
+int tempoBluGiallo = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,22 +17,33 @@ pinMode(rosso, OUTPUT);
 pinMode(giallo, OUTPUT);
 pinMode(blu, OUTPUT);
 pinMode(verde, OUTPUT);
+
+tempoBluGiallo   = Seriale ("Quanti millisecondi devono rimanere accesi i led blu e giallo?", tempoBluGiallo);
+tempoRossoVerde  = Seriale ("Quanti millisecondi devono rimanere accesi i led rosso e verde?", tempoRossoVerde);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-Sequenza(rosso, verde, giallo, blu);
-delay(2000);
-Sequenza(giallo, blu, rosso, verde);
-delay(2000);
+  
+SequenzaLed(rosso, verde, giallo, blu);
+delay(tempoBluGiallo);
+SequenzaLed(giallo, blu, rosso, verde);
+delay(tempoRossoVerde);
+
 }
 
-void Sequenza(int led, int led1, int led2, int led3){
+
+void SequenzaLed(int led, int led1, int led2, int led3){ //metodo per la sequenza di accensione/spegnimento dei led
 digitalWrite(led, LOW);
 digitalWrite (led1, LOW);
 digitalWrite (led2, HIGH);
 digitalWrite (led3, HIGH);
 }
 
-
+int Seriale(String frase, int var){  //Metodo per richiedere in seriale la durata dei led
+  Serial.println(frase); 
+  while (Serial.available() == 0) {};
+  var = Serial.readString().toInt();
+  return var;
+}
 
