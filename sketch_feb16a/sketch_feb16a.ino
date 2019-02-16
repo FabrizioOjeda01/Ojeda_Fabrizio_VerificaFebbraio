418df9e550ca61428dc63fcc66e36d381f609577
Fabrizio <fabrizioojeda@hotmail.it>
2019-02-16 08:23:29 +0100

    Creazione verifica

---
+++ sketch_feb16a/sketch_feb16a.ino
@@ -0,0 +1,18 @@
+int rossso = 12;
+int giallo = 6;
+int blu    = 8;
+int verde  = 10;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(rosso, OUTPUT);
+pinMode(giallo, OUTPUT);
+pinMode(blu, OUTPUT);
+pinMode(verde, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
